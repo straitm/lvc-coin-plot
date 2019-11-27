988315785d6c341c753fd44bd700182f8c8b5210
Matthew Strait <mstrait@fnal.gov>
2019-11-27 10:10:08 -0600

    make writebgfile.sh more of a proper script

--- writebgfile.sh
+++ writebgfile.sh
@@ -2,4 +2,23 @@
 
 e=$(basename $PWD | cut -d- -f 2)
 
-(../ligopass2/ligopass2.sh 114x-neardet-ddactivity1.hadded.root; ../ligopass2/ligopass2.sh 109x-fardet-t02.hadded.root) | grep TRACKS | awk '{if(NF == 2) print $0}' | grep -v ^TRACKS_POINT_0_FDMINBIAS_P | grep -v ^HALFCONTAINED_TRACKS_POINT_1_FDMINBIAS_P | sed 's/ *$//' | tee ../ligopass2/$e.bg
+rm -f *x-neardet-ddactivity1.hadded.root *x-neardet-t02.hadded.root
+
+nnd=$(ls *ddactivity1.hadded.root | wc -l)
+nfd=$(ls         *t02.hadded.root | wc -l)
+
+echo $nnd ND files and $nfd FD files
+
+ndhadded=${nnd}x-neardet-ddactivity1.hadded.root
+fdhadded=${nfd}x-fardet-t02.hadded.root
+
+if ! hadd $ndhadded *ddactivity1.hadded.root; then exit 1; fi
+if ! hadd $fdhadded *t02.hadded.root; then exit 1; fi
+
+(../ligopass2/ligopass2.sh $ndhadded; ../ligopass2/ligopass2.sh $fdhadded) | \
+  grep TRACKS | \
+  awk '{if(NF == 2) print $0}' | \
+  grep -v ^TRACKS_POINT_0_FDMINBIAS_P | \
+  grep -v ^HALFCONTAINED_TRACKS_POINT_1_FDMINBIAS_P | \
+  sed 's/ *$//' | \
+  tee ../ligopass2/$e.bg
